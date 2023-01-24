59879107724e9c85ec6475d1fa5f89133044004e
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-01-24 10:20:39 +0600

    Example of cube(), average() function in operation.py

--- operation.py
+++ operation.py
@@ -1,7 +1,9 @@
-from calc import add, multiply, subtract, divide, square
+from calc import add, multiply, subtract, divide, square, cube, average, exponent
 
 print(add(10, 5))
 print(multiply(10, 5))
 print(subtract(10, 5))
 print(divide(10, 5))
 print(square(5))
+print(cube(5))
+print(average([4, 4, 4, 4]))
